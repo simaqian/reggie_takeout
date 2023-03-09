ff86d3b619263af5d6363e7736b6b4debfd6099b
simaq <869363123@qq.com>
2023-03-09 17:42:13 +0800

    swagger功能（已经注释掉）

--- src/main/java/com/smq/config/WebMvcConfig.java
+++ src/main/java/com/smq/config/WebMvcConfig.java
@@ -12,12 +12,16 @@ import java.util.List;
 
 @Configuration
 @Slf4j
+//@EnableSwagger2
+//@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+//        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
     @Override
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -27,4 +31,22 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //将上面的消息转换器对象追加到mvc框架的转换器集合当中(index设置为0，表示设置在第一个位置，避免被其它转换器接收，从而达不到想要的功能)
         converters.add(0, messageConverter);
     }
+
+//    @Bean
+//    public Docket createRestApi() {
+//        //文档类型
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.smq.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("瑞吉外卖")
+//                .version("1.0")
+//                .description("瑞吉外卖接口文档")
+//                .build();
+//    }
 }
